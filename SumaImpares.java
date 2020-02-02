91e251502c9ab5a7c57d13b3f3ec6f8a3774f9dd
DanielPinzon30 <60560817+DanielPinzon30@users.noreply.github.com>
2020-02-01 22:14:11 -0500

    Add files via upload

---
+++ SumaImpares.java
@@ -0,0 +1,17 @@
+
+package sumaimpares;
+
+
+public class SumaImpares {
+    public static void main(String[] args) {
+        System.out.println("Suma de numero simpares hasta 100.000.000");
+        long x,suma=0;
+        for(x=0;x<=100000000;x++){
+        if(x%2!=0){
+        suma+=x;
+        }else{}
+        
+        }
+        System.out.println(suma);
+        }
+    }
